f8abd6dcbc56914cb4e2eb066d9a77aaa97a4758
Alexander Ivankov <alexanderivankoff@gmail.com>
2019-03-17 21:33:39 +0300

    feat: init Scene component

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,21 @@
 
 import React from 'react';
 import GphApiClient from 'giphy-js-sdk-core';
-import logo from './logo.svg';
 import styles from './App.module.scss';
+import { Scene } from './components/Scene';
 
 const giphyClientSDK = GphApiClient(process.env.REACT_APP_GIPHY_API_KEY);
 giphyClientSDK.random('gifs', {})
-  .then((response) => {
+  .then((response: {}) => {
     console.log(response);
   })
-  .catch((err) => {
+  .catch((err: {}) => {
     console.log(err);
   });
 
-const App = () => (
+const App = (): React$Node => (
   <div className={styles.App}>
-    <header className={styles['App-header']}>
-      <img src={logo} className={styles['App-logo']} alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a className={styles['App-link']} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-      </a>
-    </header>
+    <Scene />
   </div>
 );
 
---
+++ src/components/Scene/index.jsx
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+
+export const Scene = (): React$Node => (
+  <div>
+    <img
+      src="https://media1.giphy.com/media/qnTHWNey0c86c/giphy.gif"
+      style={{ width: '100vw', height: '100vh' }}
+      alt="gif"
+    />
+  </div>
+);
